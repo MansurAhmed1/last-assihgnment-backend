9ec69217859288507dbc374b92222789eaec1f84
MansurAhmed1 <mansurahmed75236@gmail.com>
2022-05-26 18:32:00 +0600

    stripeerror

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5000;
 // app.use(express.json());
 
 const corsConfig = {
-  origin: '',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
